e878277178882d2480a9c767db35373cdbd8e6df
laurenap <104783934+laurenap@users.noreply.github.com>
2022-08-11 10:45:02 -0400

    Initial Commit

---
+++ MiniAppProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MiniAppProject
+//
+//  Created by Lauren Pearson on 2022-08-11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
