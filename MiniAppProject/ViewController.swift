49231400858bf6ccca3a903b38968c87a2214332
laurenap <104783934+laurenap@users.noreply.github.com>
2022-08-17 10:52:53 -0400

    About me

--- MiniAppProject/ViewController.swift
+++ MiniAppProject/ViewController.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var factsAboutme: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func aboutMebutton(_ sender: Any) {
+        factsAboutme.text = "Hey, my name is Lauren and I'm 18 years old! I'm from Canada and I love technology + reading"
+    }
+    
 }
 
